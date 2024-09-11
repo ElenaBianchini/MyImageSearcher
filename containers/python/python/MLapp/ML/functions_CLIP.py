76c32f2242f9b14c04e0a34d506c530f6db77471
ElenaBianchini <elenabianchini_@hotmail.it>
2024-09-11 15:12:37 +0200

    Fix ML (ora funziona)

--- containers/python/python/MLapp/ML/functions_CLIP.py
+++ containers/python/python/MLapp/ML/functions_CLIP.py
@@ -1,50 +1,50 @@
-from transformers import CLIPProcessor, CLIPModel
+from transformers import AutoTokenizer, BertModel, BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 import torch
 import base64
 from io import BytesIO
 
 
+def get_text_embedding(text):
+    tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
+    model = BertModel.from_pretrained("google-bert/bert-base-uncased")
+
+    inputs = tokenizer(text, return_tensors='pt')
+    with torch.no_grad():
+        outputs = model(**inputs)
+    
+    # Restituisce l'ultimo hidden state medio (pooling di tutti i token)
+    return outputs.last_hidden_state.mean(dim=1)
+
+
 def create_embedding(img_base64):
     # Importo il modello preallenato
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-
-    # Preprocesso immagine per CLIP
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+    
+    # Preprocesso immagine per BLIP
     image_data = base64.b64decode(img_base64)
     image = Image.open(BytesIO(image_data))
-    inputs = processor(images=image, return_tensors="pt")
+    inputs = processor(image, return_tensors="pt")
+    
+    # Generare la didascalia
+    caption = model.generate(**inputs)
+    
+    # Convertire il risultato in testo
+    output = processor.decode(caption[0], skip_special_tokens=True)
+    print(output)
 
-    # Calcolo l'embedding dell'immagine
-    with torch.no_grad():
-        image_features = model.get_image_features(**inputs)
-
-    # Normalizzo l'embedding dell'immagine
-    image_features = image_features / image_features.norm(p=2, dim=-1, keepdim=True)
-
-    return image_features
+    return get_text_embedding(output)
 
 
 def most_similar_images(query, image_id_embeddings, threshold = 0.3):
-    # Importo il modello preallenato
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-
-    # Preprocesso il testo per CLIP
-    text_inputs = processor(text=[query], return_tensors="pt", padding=True)
-
-    # Calcolo l'embedding del testo
-    with torch.no_grad():
-        text_features = model.get_text_features(**text_inputs)
-
-    # Normalizzo l'embedding del testo
-    text_features = text_features / text_features.norm(p=2, dim=-1, keepdim=True)
+    text_features = get_text_embedding(query)
     
     similarities = []
     cosSimFunc = torch.nn.CosineSimilarity()
 
     for i, embed in enumerate(image_id_embeddings):
-        s = cosSimFunc(text_features, torch.Tensor(embed[1]))
+        s = cosSimFunc(text_features, embed[1])
         s = s.item()
         similarities.append((i, s))
     
@@ -61,14 +61,7 @@ def most_similar_images(query, image_id_embeddings, threshold = 0.3):
 
 
 # TEST:
-image_path = "./dog.jpeg"
-with open(image_path, "rb") as img_file:
-    img_base64 = base64.b64encode(img_file.read()).decode('utf-8')
-
-emb = create_embedding(img_base64)
-
-id_embs = [(15, emb)]
-
+id_embs = []
 image_path = "./palme.jpg"
 with open(image_path, "rb") as img_file:
     img_base64 = base64.b64encode(img_file.read()).decode('utf-8')
@@ -76,7 +69,16 @@ with open(image_path, "rb") as img_file:
 emb = create_embedding(img_base64)
 
 id_embs.append((5,emb))
-r = most_similar_images('', id_embs)
+
+image_path = "./dog.jpeg"
+with open(image_path, "rb") as img_file:
+    img_base64 = base64.b64encode(img_file.read()).decode('utf-8')
+
+emb = create_embedding(img_base64)
+
+id_embs.append((15, emb))
+
+r = most_similar_images('house', id_embs)
 print(r)
 
 
